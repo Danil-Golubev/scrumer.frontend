43fa2999ac032052cbc680412a49e836758e0a47
Danil-Golubev <dokers190rus@mail.ru>
2025-03-22 16:58:02 +0300

    mini mvp

--- src/redux/axios.ts
+++ src/redux/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:4444",
+  baseURL: "https://scrumer-backend.onrender.com",
 });
 
 //  baseURL: "http://localhost:4444"
